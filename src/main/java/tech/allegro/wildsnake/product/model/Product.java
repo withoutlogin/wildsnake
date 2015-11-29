0c0cded43e131b6669c519ab4a54a5dca9846246
Szymon <predefined77@gmail.com>
2015-11-29 23:24:42 +0100

    dodanie equalsa i hashcode'a

--- src/main/java/tech/allegro/wildsnake/product/model/Product.java
+++ src/main/java/tech/allegro/wildsnake/product/model/Product.java
@@ -32,6 +32,29 @@ public class Product {
         return imageUrl;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (id != product.id) return false;
+        if (name != null ? !name.equals(product.name) : product.name != null) return false;
+        if (imageUrl != null ? !imageUrl.equals(product.imageUrl) : product.imageUrl != null) return false;
+        return !(description != null ? !description.equals(product.description) : product.description != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (imageUrl != null ? imageUrl.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
     public String getDescription() {
         return description;
     }
